348b0d9f7c5257f709e2931de41200f854dea736
Lernni <Lernni@users.noreply.github.com>
2021-12-11 19:22:53 +0100

    wip: prevent too many connections

--- src/modules/useSocketIo.ts
+++ src/modules/useSocketIo.ts
@@ -7,11 +7,12 @@ export const useSocketIo = (backendUrl: string, namespace: `/${string}`) => {
   // socket.io reuses the existing instance based on same scheme/port/host, and we initialize sockets for each namespace.
   const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(`${backendUrl}${namespace}`)
 
-  // if (import.meta.hot) {
-  //   import.meta.hot.dispose((data) => {
-  //     socket.disconnect()
-  //   })
-  // }
+  // TODO: prevent clients from connecting multiple times
+  if (import.meta.hot) {
+    import.meta.hot.dispose((data) => {
+      socket.disconnect()
+    })
+  }
 
   const reconnect = () => {
     socket.connect()
