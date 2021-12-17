b48d91c0644ebeb106546ced7266de236ab4f733
Julian Meinking <s80448@htw-dresden.de>
2021-12-17 23:12:38 +0100

    feat: zoom playArea
    Co-authored-by: Lenny <Lernni@users.noreply.github.com>
    Co-authored-by: Raphael Rezaii-Djafari <Rezaii-Djafari@users.noreply.github.com>

--- src/views/InGame.vue
+++ src/views/InGame.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue'
-import { useDraggable, useElementBounding } from '@vueuse/core'
+import { ref, computed } from 'vue'
+import { useDraggable, useElementBounding, onKeyStroke, useCssVar } from '@vueuse/core'
 
 const el = ref<HTMLElement | null>(null)
 const playAreaRef = ref<HTMLElement | null>(null)
@@ -12,6 +12,26 @@ const { x, y } = useDraggable(el, {
   exact: true,
   initialValue: { x: 40, y: 40 },
 })
+
+const zoomVar = useCssVar('--zoom', playAreaRef)
+const zoomPercent = computed(() => {
+  return Number.parseInt(zoomVar.value.replace('%', ''))
+})
+
+onKeyStroke('+', () => {
+  zoomVar.value = `${zoomPercent.value + 10}%`
+})
+onKeyStroke('0', () => {
+  zoomVar.value = '100%'
+})
+onKeyStroke('-', () => {
+  // zooming out of playarea is not permitted
+  if (zoomPercent.value <= 100) return
+
+  zoomVar.value = `${zoomPercent.value - 10}%`
+})
+
+const dynamicFontSize = computed(() => `${(zoomPercent.value / 100) * 1.25}rem`)
 
 // calculate the percentage for the position of objects
 // useDraggable requires coordinates but the objects are arranged with percentages
@@ -31,14 +51,23 @@ const position = computed(() => {
 
 <template>
   <div class="overflow-auto h-full bg-gray-200">
-    <div ref="playAreaRef" class="flex relative items-start bg-red-400 aspect-square fill-viewport">
+    <div
+      ref="playAreaRef"
+      class="flex relative items-start bg-red-400 aspect-square tt-fill-viewport"
+    >
       <div
         ref="el"
         :style="`top: ${position.y}%; left: ${position.x}%`"
-        class="absolute py-56 px-8 w-80 text-xl text-center bg-green-400 cursor-pointer select-none"
+        class="flex absolute justify-center items-center text-center bg-green-400 cursor-pointer select-none w-[20%] h-[30%] tt-dynamic-font-size"
       >
         Drag me! I am at <br />{{ position.x }}%, <br />{{ position.y }}%
       </div>
     </div>
   </div>
 </template>
+
+<style>
+.tt-dynamic-font-size {
+  font-size: v-bind(dynamicFontSize);
+}
+</style>
