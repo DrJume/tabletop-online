09ee13741942227d3320a56bce3a6dc66ec2d779
Julian Meinking <s80448@htw-dresden.de>
2021-12-18 21:50:47 +0100

    feat: use sharedb with socket.io disabled
    Co-authored-by: Lenny <Lernni@users.noreply.github.com>
    Co-authored-by: Raphael Rezaii-Djafari <Rezaii-Djafari@users.noreply.github.com>

--- backend/index.ts
+++ backend/index.ts
@@ -1,51 +1,73 @@
 import { createServer } from 'http'
-// import { Server } from 'socket.io'
-// import { instrument } from '@socket.io/admin-ui'
+const httpServer = createServer()
 
-// import { ClientToServerEvents, ServerToClientEvents } from './types/socketIo'
+// *** init websockets ***
 
+// init sharedb socket
 import ShareDB from 'sharedb'
 import WebSocket from 'ws'
 import WebSocketJSONStream from '@teamwork/websocket-json-stream'
 
-const httpServer = createServer()
-
-// const io = new Server<ClientToServerEvents, ServerToClientEvents>(httpServer, {
-//   cors: {
-//     origin: ['http://localhost:3000', 'https://admin.socket.io'],
-//     credentials: true,
-//   },
-// })
-
-// // init admin panel, access only with /admin
-// instrument(io, {
-//   auth: false,
-// })
-
-const webSocketServer = new WebSocket.Server({ server: httpServer })
-
+const shareDBSocketServer = new WebSocket.Server({ noServer: true })
 const backend = new ShareDB()
-webSocketServer.on('connection', (webSocket) => {
+
+// init socket.io socket
+import { Server as SocketIOServer } from 'socket.io'
+import { ClientToServerEvents, ServerToClientEvents } from './types/socketIo'
+import { instrument } from '@socket.io/admin-ui'
+
+const socketIOSocketServer = new SocketIOServer<ClientToServerEvents, ServerToClientEvents>(
+  httpServer,
+  {
+    path: '/socket.io',
+    cors: {
+      origin: ['http://localhost:3000', 'https://admin.socket.io'],
+      credentials: true,
+    },
+  }
+)
+
+// *** additional config ***
+
+// init admin panel, access only with /admin
+instrument(socketIOSocketServer, {
+  auth: false,
+})
+
+httpServer.on('upgrade', function upgrade(request, socket, head) {
+  // socketIOSocketServer upgrades are handled by express
+  if (!request.url.startsWith('/socket.io')) {
+    shareDBSocketServer.handleUpgrade(request, socket, head, function done(ws) {
+      shareDBSocketServer.emit('connection', ws, request)
+    })
+  }
+})
+
+// *** sharedb server logic ***
+
+shareDBSocketServer.on('connection', (webSocket) => {
   const stream = new WebSocketJSONStream(webSocket)
   backend.listen(stream)
 })
 
-// // tabletop server logic
-// io.of('/tabletop').on('connection', (socket) => {
-//   console.log('user ' + socket.id + ' connected')
+// *** tabletop server logic ***
 
-//   socket.on('disconnect', () => {
-//     console.log('user ' + socket.id + ' disconnected')
-//   })
-// })
+socketIOSocketServer.of('/tabletop').on('connection', (socket) => {
+  console.log('user ' + socket.id + ' connected')
+
+  socket.on('disconnect', () => {
+    console.log('user ' + socket.id + ' disconnected')
+  })
+})
 
 // start server
 httpServer.listen(8080, () => {
   console.log('listening on *:8080')
 })
 
+// TODO: check if this is actually working
 // force clean reconnect of all active sockets when using nodemon
 process.once('SIGUSR2', () => {
-  // io.disconnectSockets(true)
+  socketIOSocketServer.disconnectSockets(true)
   process.kill(process.pid, 'SIGUSR2')
 })
--- src/views/InGame.vue
+++ src/views/InGame.vue
@@ -1,74 +1,73 @@
 <script setup lang="ts">
-import { computed, ComputedRef, onMounted, reactive, ref } from 'vue'
+import { ref } from 'vue'
 import { useDraggable } from '@vueuse/core'
 
+// sharedb socket
 import ReconnectingWebSocket from 'reconnecting-websocket'
 import { Connection } from 'sharedb/lib/client'
 import type { Socket } from 'sharedb/lib/sharedb'
-
-// import { useTabletopSocket } from '@/modules/useTabletopSocket'
-
-// const { onLock, doLock, drag, onMove } = useTabletopSocket('http://localhost:8080')
-
-// let locked = false
-
-// onLock((lockValue: boolean) => {
-//   locked = lockValue
-// })
 
 const socket = new ReconnectingWebSocket('ws://localhost:8080')
 const connection = new Connection(socket as Socket)
 
 const doc = connection.get('doc-collection', 'doc-id')
 
+const isVisible = ref(false)
+
 doc.subscribe((error) => {
   if (error) return console.error(error)
 
+  console.log('subscribe', JSON.stringify(doc.data))
+
   // If doc.type is undefined, the document has not been created, so let's create it
   if (!doc.type) {
-    doc.create({ x: 0, y: 0 }, (error) => {
+    doc.create({ idCounter: 0, objects: {} }, (error) => {
+      console.log('doc created')
       if (error) console.error(error)
+
+      // create new card and increment idCounter
+      doc.submitOp([
+        { p: ['objects', doc.data.idCounter], oi: { x: 0, y: 0, isLocked: false } },
+        { p: ['idCounter'], na: 1 },
+      ])
     })
   }
+
+  if (doc.data.objects['0']) {
+    updateCard()
+    isVisible.value = true
+  }
+
+  // combine all operations into one with 'op batch'
+  doc.on('op batch', (op) => {
+    if (!isVisible.value) isVisible.value = true
+
+    console.log('op batch', op)
+    updateCard()
+  })
 })
 
 // draggable card
 const el = ref<HTMLElement | null>(null)
-let positionRelative: ComputedRef<{ x: number; y: number }>
 
-onMounted(() => {
-  if (el.value === null) throw new Error('could not get ref of draggable card')
-  const container = el.value.parentElement
-  if (container === null) throw new Error('could not get ref of draggable card container')
+const hasDragged = ref(false)
 
-  const { width: containerWidth, height: containerHeight } = container.getBoundingClientRect()
-
-  doc.on('op', (op) => {
-    positionClientAbsolute.value.x = (doc.data.x / 100) * containerWidth
-    positionClientAbsolute.value.y = (doc.data.y / 100) * containerHeight
-  })
-
-  positionRelative = computed(() => {
-    return {
-      x: (positionClientAbsolute.value.x / containerWidth) * 100,
-      y: (positionClientAbsolute.value.y / containerHeight) * 100,
-    }
-  })
-})
 // `style` will be a helper computed for `left: ?px; top: ?px;`
-const { position: positionClientAbsolute, style } = useDraggable(el, {
+const { x, y, style } = useDraggable(el, {
   exact: true,
-  // TODO: make coordinates procentual and relative to screen size
-  initialValue: { x: 40, y: 40 },
+  initialValue: { x: 0, y: 0 },
 
   // Callback when the dragging starts. Return `false` to prevent dragging.
   onStart() {
-    // if (locked) {
-    //   return false
-    // } else {
-    //   // doLock(true)
-    // }
-    console.log('start')
+    if (doc.data.objects['0'].isLocked) {
+      console.log('start drag blocked')
+
+      return false
+    }
+    hasDragged.value = true
+    doc.submitOp([{ p: ['objects', '0', 'isLocked'], oi: true }])
+
+    console.log('start card drag')
   },
 
   // Callback during dragging.
@@ -76,32 +75,39 @@ const { position: positionClientAbsolute, style } = useDraggable(el, {
     // TODO: throttle down the amount of messages sent
 
     doc.submitOp([
-      { p: ['x'], oi: positionRelative.value.x },
-      { p: ['y'], oi: positionRelative.value.y },
+      { p: ['objects', '0', 'x'], oi: x.value },
+      { p: ['objects', '0', 'y'], oi: y.value },
     ])
   },
 
   // Callback when dragging end.
   onEnd() {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    // doLock(false)
-    // console.log('end')
+    if (!hasDragged.value) return
+
+    console.log('end card drag')
+    doc.submitOp([{ p: ['objects', '0', 'isLocked'], oi: false }])
+    hasDragged.value = false
   },
 })
 
-// onMove((position: any) => {
-//   console.log(position)
-//   x.value = Number.parseInt(position.x)
-//   y.value = Number.parseInt(position.y)
-// })
+const updateCard = () => {
+  console.log('update card', JSON.stringify(doc.data))
+  x.value = doc.data.objects['0'].x
+  y.value = doc.data.objects['0'].y
+}
+
+doc.on('load', () => {
+  console.log('load', JSON.stringify(doc.data))
+})
 </script>
 
 <template>
   <div
+    v-show="isVisible"
     ref="el"
     :style="style"
     class="fixed py-56 px-8 w-80 text-xl text-center bg-red-400 cursor-pointer select-none"
   >
-    Drag me! I am at {{ positionRelative.y }}, {{ positionRelative.y }}
+    Drag me! I am at {{ x }}, {{ y }}
   </div>
 </template>
